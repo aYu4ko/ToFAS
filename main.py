77a35ed3af2dbe8e73187677941e5e6f78201c70
rimrim <rimrimrimrim1987@gmail.com>
2025-08-14 17:44:23 +0800

    stricter move back loop detection

--- main.py
+++ main.py
@@ -288,7 +288,7 @@ class RimInputScheduler:
             # Process incoming queue
             # Note that this is only processed when main queue is empty
             if not self.incoming_queue.empty():
-                if move_back_count >= 1000:
+                if move_back_count >= 2000:
                     raise ValueError("Move back loop detected!")
                 else:
                     await asyncio.sleep(0.1)
